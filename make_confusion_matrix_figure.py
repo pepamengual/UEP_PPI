2194c201f31be651bc89651dc22af7ad48ab69c9
pepamengual <pepamengualrigo@gmail.com>
2020-02-13 19:55:59 +0100

    nice plot of confusion matrix scheme multiplot

--- make_confusion_matrix_figure.py
+++ make_confusion_matrix_figure.py
@@ -1,19 +1,26 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import numpy as np
 
 def create_figure(data):
     fig, ax = plt.subplots(4, 2)
+    vertical_lines_coordinates = [0.25, 0.48, 0.71, 0.73, 0.99]
+    horizontal_lines_coordinates = [0.25, 0.62, 0.99]
     
-    ax[0,0].axvline(x=0.01, ymax=0.98, color="black")
-    ax[0,0].axvline(x=0.49, ymax=0.98, color="black")
-    ax[0,0].axvline(x=0.99, ymax=0.98, color="black")
-    ax[0,0].axhline(y=0.01, xmax=0.98, color="black")
-    ax[0,0].axhline(y=0.49, xmax=0.98, color="black")
-    ax[0,0].axhline(y=0.99, xmax=0.98, color="black")
-
-    #ax[0,0].add_patch(patches.Rectangle((0.01, 0.01), 0.98, 0.98, fill=False)) #fill false removes background
-
-
+    mono = {'family': 'monospace', 'fontsize': 13}
+    for axis in ax.flat:
+        for vertical in vertical_lines_coordinates:
+            axis.axvline(x=vertical, ymin=vertical_lines_coordinates[0], ymax=vertical_lines_coordinates[-1], color="black")
+        for horizontal in horizontal_lines_coordinates:
+            axis.axhline(y=horizontal, xmin=vertical_lines_coordinates[0], xmax=vertical_lines_coordinates[2], color="black")
+            axis.axhline(y=horizontal, xmin=vertical_lines_coordinates[3], xmax=vertical_lines_coordinates[4], color="black")
+        
+        axis.text(x=np.mean(vertical_lines_coordinates[:2]), y=1.05, s="+", horizontalalignment='center', fontdict=mono) # + symbol
+        axis.text(x=np.mean(vertical_lines_coordinates[1:3]), y=1.05, s="-", horizontalalignment='center', fontdict=mono) # - symbol
+        axis.text(x=np.mean(vertical_lines_coordinates[3:]), y=1.05, s="MCC", horizontalalignment='center', fontdict=mono) # MCC symbol
+        axis.text(x=0.15, y=np.mean(horizontal_lines_coordinates[1:]), s="+ Pred", horizontalalignment='center', verticalalignment='center', fontdict=mono) # + Pred symbol
+        axis.text(x=0.15, y=np.mean(horizontal_lines_coordinates[:2]), s="- Pred", horizontalalignment='center', verticalalignment='center', fontdict=mono) # - Pred symbol
+        axis.text(x=0, y=np.mean([horizontal_lines_coordinates[0], horizontal_lines_coordinates[-1]]), s="UEP", horizontalalignment='center', verticalalignment='center', fontdict=mono, rotation="vertical")
     [axis.set_axis_off() for axis in ax.ravel()] #set all axis off
     plt.show()
 
